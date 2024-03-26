fb9108bd9c9feb2a45e5e3ab03d5d50fc9b86258
AndreaB-2 <andrea.barcenas-1@ou.edu>
2024-03-26 10:14:56 -0500

    LC

---
+++ 0326/1.js
@@ -0,0 +1,51 @@
+"use strict";
+
+let R = null;
+let V = 0;
+
+//async: fetch()
+//sync: 
+
+async function loade()
+{
+    let url = `https://excuser-three.vercel.app/v1/excuse/college/`;
+
+    let r = await fetch(url);
+    let rj = await r.json();
+
+    R = rj;
+    console.log(rj);
+
+    let d3 = document.querySelector("#d3");
+    d3.innerHTML = rj[0].excuse;
+
+    let opts =
+    {
+        title: "Success",
+        text: "Excuse loaded",
+        icon: "success",
+
+    };
+    Swal.fire(opts);
+
+    V = V + 10;
+    opts =
+    {
+        targets: [d3],
+        //translateX: { value: V, duration: 3000, },
+        //rotate: { value: V, duration: 3000, },
+        scale: { value: V/100, duration: 3000, },
+
+
+    };
+    anime(opts);
+
+
+}
+
+let b3 = document.querySelector("#b3");
+
+b3.addEventListener("click",loade);
+
+
+
